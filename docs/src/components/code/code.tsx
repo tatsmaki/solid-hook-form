c26b3d58f9649e8677a73dd0ab8c0d5a77460cd5
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2025-10-19 02:51:07 +0200

    fix: prism esm ssr

--- docs/src/components/code/code.tsx
+++ docs/src/components/code/code.tsx
@@ -1,20 +1,24 @@
-import Prism from "prismjs";
-import loadLanguages from "prismjs/components/";
+import { Prism } from "prism-esm";
+import { loader as typescriptLoader } from "prism-esm/components/prism-typescript.js";
+import { loader as bashLoader } from "prism-esm/components/prism-bash.js";
 import { CodeProps } from "./code.types";
 import sx from "./code.module.css";
 import "./highlight.css";
 
-loadLanguages(["ts", "sh"]);
+const prism = new Prism();
+
+typescriptLoader(prism);
+bashLoader(prism);
 
 const languages = {
-  js: Prism.languages.js,
-  ts: Prism.languages.ts,
-  sh: Prism.languages.sh,
+  js: prism.languages.js,
+  ts: prism.languages.ts,
+  sh: prism.languages.sh,
 };
 
 export const Code = (props: CodeProps) => {
   const language = props.language;
-  const html = Prism.highlight(props.children, languages[language], language);
+  const html = prism.highlight(props.children, languages[language], language);
 
   return <code class={sx.code} innerHTML={html} />;
 };
