fb12a2ae03106a0e25c10ba8e37f0e7dc09e99b4
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2025-11-03 00:37:59 +0100

    build: switch to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, Options } from "tsup";
+import { solidPlugin } from "esbuild-plugin-solid";
+
+const config: Options = {
+  entry: ["src/main.ts"],
+  target: "esnext",
+  platform: "browser",
+  format: ["esm"],
+  clean: true,
+  outDir: "dist/",
+  treeshake: {
+    preset: "smallest",
+  },
+  replaceNodeEnv: true,
+  external: ["react"],
+};
+
+export default defineConfig([
+  {
+    ...config,
+    dts: false,
+    esbuildOptions(options) {
+      options.jsx = "preserve";
+      options.chunkNames = "[name]/[hash]";
+      options.drop = ["console", "debugger"];
+    },
+    outExtension() {
+      return {
+        js: ".jsx",
+      };
+    },
+  },
+  {
+    ...config,
+    dts: true,
+    esbuildOptions(options) {
+      options.chunkNames = "[name]/[hash]";
+      options.drop = ["console", "debugger"];
+    },
+    esbuildPlugins: [solidPlugin({ solid: { generate: "dom" } })],
+  },
+]);
