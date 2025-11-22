33e2d0ad581f6f82d13cc27aefac3b29a4f08297
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2025-11-22 14:32:33 +0100

    feat: add touched fields

---
+++ src/logic/create_touched.ts
@@ -0,0 +1,29 @@
+import { createSignal } from "solid-js";
+import { FormValues } from "../types/form";
+import { set } from "../utils/set";
+import { Path } from "../types/path";
+import { TouchedFields } from "../types/touched";
+
+export const createTouched = <F extends FormValues>() => {
+  const [touchedFields, setTouchedFields] = createSignal<TouchedFields<F>>({});
+
+  const addTouched = (name: Path<F>) => {
+    setTouchedFields((prev) => {
+      const newState = { ...prev };
+
+      set(newState, name, true);
+
+      return newState;
+    });
+  };
+
+  const resetTouched = (keepTouched?: boolean) => {
+    if (keepTouched) {
+      return;
+    }
+
+    setTouchedFields({});
+  };
+
+  return { touchedFields, addTouched, resetTouched };
+};
