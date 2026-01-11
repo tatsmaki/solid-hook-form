6e05160e3b0a3c683234cba7b0170d6f51065d81
Rostislav Harlanov <thorn_pear@icloud.com>
2026-01-11 15:32:33 +0100

    fix: get rule by name

--- src/logic/format_value.ts
+++ src/logic/format_value.ts
@@ -2,8 +2,8 @@ import type { Path } from "react-hook-form";
 import type { FormValues } from "../types/form";
 import type { Rules } from "../types/validate";
 
-export const formatValue = <F extends FormValues, T>(value: T, rules: Rules<F, Path<F>>) => {
-  if (rules.valueAsNumber) {
+export const formatValue = <F extends FormValues, T>(value: T, rules?: Rules<F, Path<F>>) => {
+  if (rules?.valueAsNumber) {
     return Number(value);
   }
 
